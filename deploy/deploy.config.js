ae7c89ba8a1e6988f5cb2850276c4b72bca3bf89
liuqi <741365564@qq.com>
2020-01-19 09:41:50 +0800

    [新增] 服务器相关配置

---
+++ deploy/deploy.config.js
@@ -0,0 +1,38 @@
+const deployConfig = {
+  projectName: '', // 项目名称
+  dev: { // 测试环境
+    name: '测试环境',
+    script: 'npm run build', // 测试环境打包脚本
+    distPath: './dist', // 本地打包dist目录
+    server: {
+      host: '', // 测试服务器地址
+      port: 22, // ssh port，一般默认22
+      username: '', // 登录服务器用户名
+      password: '', // 登录服务器密码
+      privateKey: '', // 本地私钥地址，位置一般在C:/Users/xxx/.ssh/id_rsa，非必填，有私钥则配置
+      passphrase: '', // 本地私钥密码，非必填，有私钥则配置
+      webDir: '' // // 测试环境服务器地址
+    },
+    oss: {}
+  },
+  prod: { // 线上环境
+    name: '线上环境',
+    script: 'npm run build', // 线上环境打包脚本
+    distPath: './dist', // 本地打包dist目录
+    server: {
+      host: '', // 测试服务器地址
+      port: 22, // ssh port，一般默认22
+      username: '', // 登录服务器用户名
+      password: '', // 登录服务器密码
+      privateKey: '', // 本地私钥地址，位置一般在C:/Users/xxx/.ssh/id_rsa，非必填，有私钥则配置
+      passphrase: '', // 本地私钥密码，非必填，有私钥则配置
+      webDir: '' // // 测试环境服务器地址
+    },
+    oss: {}
+  }
+  // 再还有多余的环境按照这个格式写即可
+}
+
+module.exports = {
+  deployConfig
+}
